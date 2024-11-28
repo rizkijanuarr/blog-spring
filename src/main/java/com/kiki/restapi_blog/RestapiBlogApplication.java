4abcbfb42b6c09abe56ea9744adb8b377175b129
Kiki <id.rizkijanuar@gmail.com>
2024-11-28 12:02:00 +0700

    🎉 Initial commit

---
+++ src/main/java/com/kiki/restapi_blog/RestapiBlogApplication.java
@@ -0,0 +1,13 @@
+package com.kiki.restapi_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiki/restapi_blog/RestapiBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiki.restapi_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
